7a0c77149119ff9f244bed84dbd8b1dd4634e464
Daniel Lopes <danielvlopes@gmail.com>
2015-02-21 18:25:00 -0600

    adding sinatra/bundler and redis

---
+++ api.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+# POST /tag
+
+# - Entity Type, e.g. 'Product', 'Article'
+# - Entity Identifier, e.g. '1234', '582b5530-6cdb-11e4-9803-0800200c9a66'
+# - Tags, e.g. ['Large', 'Pink', 'Bike']
+
+post '/tag' do
+  "Hello World!"
+end
