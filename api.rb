7a076cabb4e583383f3d2664a3dad785ab9876e3
Daniel Lopes <danielvlopes@gmail.com>
2015-02-21 18:46:14 -0600

    first iteration of the post tag api

--- api.rb
+++ api.rb
@@ -1,11 +1,41 @@
 require 'sinatra'
+require 'json'
 
-# POST /tag
+before do
+  @redis = Redis.new
+  content_type :json
+end
 
 # - Entity Type, e.g. 'Product', 'Article'
 # - Entity Identifier, e.g. '1234', '582b5530-6cdb-11e4-9803-0800200c9a66'
 # - Tags, e.g. ['Large', 'Pink', 'Bike']
 
 post '/tag' do
-  "Hello World!"
+  require_params!(:type, :id, :tags)
+  key = "#{params[:type]}:#{params[:id]}:tags"
+
+  @redis.multi do
+    @redis.del(key)
+    @redis.sadd(key, params[:tags])
+  end
+
+  status 200
+  content_type :json
+  { params[:id] => params[:tags] }.to_json
+end
+
+# HELPERS
+
+# def add_counter(tags)
+#   HSET myhash field 5
+#   tags.each do |tag|
+#   end
+# end
+
+def require_params!(*keys)
+  keys.each do |key|
+    if params[key].nil? || params[key].empty?
+      halt 500, { error: "Param #{key} is required!" }.to_json
+    end
+  end
 end
